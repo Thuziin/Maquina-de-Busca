d0c5b0e04cf90a6377b710e047916629c268a88e
Jouberto1 <joubert-lino12@outlook.com>
2023-05-27 19:51:45 -0300

    criacao de vetor para contar as palavras dentro de cada arquivo

--- main.cpp
+++ main.cpp
@@ -13,13 +13,20 @@ int main () {
 	string caminho = "./documents";
 	Document arquivo(caminho);
 	vector<pair<string, vector<string>>> documentos = arquivo.DocumentReader();
+	<map<string, map<string, int> documentoCerto;
 
-	for (auto it: documentos) {
-		cout << "Arquivo: " << it.first << endl;
-		cout << "Palavras: " << endl;
+	for (const auto it: documentos) {
 		for (auto palavra: it.second) {
-			cout << palavra << endl;
+			documentoCerto[it.first][palavra]++;
 		}
+	}
+
+	for (const auto it: documentoCerto) {
+		cout << "Documento: " << it.first << endl;
+		for (const auto info: it.second) {
+			cout << "Palavra: " << info.first << " - " << "Qtd: " << info.second << endl;
+		}
+		cout << endl;
 	}
 	return 0;
 }
