5a4a785e1ad1b745d3850843f28f022ac88b6eac
Thuziin <arthuroliveira00132@gmail.com>
2023-06-01 14:24:56 -0300

    remoção da function normalizaPalavra()

--- main.cpp
+++ main.cpp
@@ -9,29 +9,6 @@ using namespace std;
 
 // Para o programa compilar é necessário usar a versão do c++17
 
-string normalizarPalavra (const string palavra) {
-    string palavraNormalizada = palavra;
-    // Transformando a palavara para minúscula
-    transform(
-        palavraNormalizada.begin(), palavraNormalizada.end(), palavraNormalizada.begin(),[](unsigned char c){
-            return tolower(c); 
-    });
-    // Removendo caracteres  !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-    palavraNormalizada.erase(
-        remove_if(palavraNormalizada.begin(), palavraNormalizada.end(), [](unsigned char c) {
-            return ispunct(c);
-        }),
-    palavraNormalizada.end());
-    // Removendo numeração
-    palavraNormalizada.erase(
-        remove_if(palavraNormalizada.begin(), palavraNormalizada.end(), [](unsigned char c) {
-            return isdigit(c);
-        }),
-    palavraNormalizada.end());
-        
-    return palavraNormalizada;
-}
-
 int main () {
 
 	string caminho = "./documents";
