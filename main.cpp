14811df2a08e4e77c947d43859b81184aa376230
Thuziin <arthuroliveira00132@gmail.com>
2023-06-01 14:15:09 -0300

    restaurando

--- main.cpp
+++ main.cpp
@@ -9,33 +9,6 @@ using namespace std;
 
 // Para o programa compilar é necessário usar a versão do c++17
 
-// Função que realiza todas as correções necessárias
-
-string normalizarPalavra (const string palavra) {
-	string palavraNormalizada = palavra;
-    // Transformando a palavara para minúscula
-    transform(
-        palavraNormalizada.begin(), palavraNormalizada.end(), palavraNormalizada.begin(),[](unsigned char c){
-            return tolower(c); 
-    });
-    // Removendo caracteres  !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-    palavraNormalizada.erase(
-        remove_if(palavraNormalizada.begin(), palavraNormalizada.end(), [](unsigned char c) {
-            return ispunct(c);
-        }),
-    palavraNormalizada.end());
-    // Removendo numeração
-    palavraNormalizada.erase(
-        remove_if(palavraNormalizada.begin(), palavraNormalizada.end(), [](unsigned char c) {
-            return isdigit(c);
-        }),
-    palavraNormalizada.end());
-    
-    // Removendo acento e cedilha
-        
-    return palavraNormalizada;
-}
-
 int main () {
 
 	string caminho = "./documents";
