6ca41a0385c69fd6c7478987bb22af6cfb39dd99
Jouberto1 <joubert-lino12@outlook.com>
2023-06-08 15:52:17 -0300

    tratamento de erro: diretório vazio

--- src/document.cpp
+++ src/document.cpp
@@ -1,10 +1,13 @@
 #include "document.h"
 
+#include <iostream>
 
 #include <filesystem>
 #include <algorithm>
 #include <fstream>
 #include <cctype>
+
+#include <cstdlib>
 
 using namespace std;
 
@@ -14,6 +17,11 @@ Document::Document (string caminho) {
 
 vector<pair<string, vector<string>>> Document::DocumentReader () {
 	vector<pair<string, vector<string>>> documentos;
+
+	try {
+		if (filesystem::is_empty(caminhoDiretorio_)) {
+			throw runtime_error("O diretório está vazio.");
+		}
 		// Iteração para acesssar cada arquivo do diretório informado
 		for (const auto& entry : filesystem::directory_iterator(caminhoDiretorio_)){
 			if (entry.is_regular_file()) {
@@ -36,5 +44,10 @@ vector<pair<string, vector<string>>> Document::DocumentReader () {
 				}
 			}
 		}
+	} catch (exception& e) {
+		cerr << "Erro: " << e.what() << endl;
+		exit(1);
+	}
+	
 	return documentos;
 }
